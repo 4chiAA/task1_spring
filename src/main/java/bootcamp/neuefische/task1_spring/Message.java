3178752c1fb0be2fd6d995d6499c244de1869f9d
Achim Lenz <hello.achim@outlook.com>
2023-11-22 12:48:31 +0100

    add controller, class for messages, methods post, get and delete messages

---
+++ src/main/java/bootcamp/neuefische/task1_spring/Message.java
@@ -0,0 +1,16 @@
+package bootcamp.neuefische.task1_spring;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    String id;
+    String name;
+    String message;
+
+
+}
--- src/main/java/bootcamp/neuefische/task1_spring/helloWorldController.java
+++ src/main/java/bootcamp/neuefische/task1_spring/helloWorldController.java
@@ -1,13 +1,45 @@
 package bootcamp.neuefische.task1_spring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class helloWorldController {
 
+    private List<Message> messageList = new ArrayList<>();
+
     @GetMapping("/hello")
     public String hello(){
         return "Hello, World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getMessages(){
+        return messageList;
+    }
+
+    @PostMapping()
+    public Message postMessage(@RequestBody Message message){
+        messageList.add(message);
+        return message;
+    }
+
+    @DeleteMapping("/messages/{id}")
+    public String deleteMessage(@PathVariable String id){
+        Message message = messageList.stream()
+                        .filter(m1 -> m1.getId().equals(id))
+                        .findFirst()
+                        .orElseThrow(() -> new NoSuchElementException("Message mit der id \"" + id + "\" wurde nicht gefunden!"));
+        messageList.removeIf(m2 -> m2.getId().equals(id));
+        return "Ihre Message \"" + message.getMessage() + "\" wurde gelöscht!";
     }
 }
